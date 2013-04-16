a49b2cb8d94005dd38540540b8cb0557e8f7819d
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-04-17 01:48:51 +0900

    Enhanced to add/edit directory.

---
+++ src/jp/gr/java_conf/neko_daisuki/simplemediascanner/EditActivity.java
@@ -0,0 +1,70 @@
+package jp.gr.java_conf.neko_daisuki.simplemediascanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class EditActivity extends Activity {
+
+    private class OkButtonOnClickListener implements View.OnClickListener {
+
+        public void onClick(View _) {
+            ok();
+        }
+    }
+
+    private class CancelButtonOnClickListener implements View.OnClickListener {
+
+        public void onClick(View _) {
+            cancel();
+        }
+    }
+
+    public static final String EXTRA_KEY_DIRECTORY = "directory";
+
+    private Directory mDirectory;
+    private EditText mDirectoryEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        mDirectoryEditText = (EditText)findViewById(R.id.directory_text);
+
+        Object o = getIntent().getSerializableExtra(EXTRA_KEY_DIRECTORY);
+        mDirectory = (Directory)o;
+        mDirectoryEditText.setText(mDirectory.path);
+
+        View okButton = findViewById(R.id.ok_button);
+        okButton.setOnClickListener(new OkButtonOnClickListener());
+        View cancelButton = findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new CancelButtonOnClickListener());
+    }
+
+    private void ok() {
+        Directory directory = new Directory();
+        directory.id = mDirectory.id;
+        directory.path = mDirectoryEditText.getText().toString();
+
+        Intent i = getIntent();
+        i.putExtra(EXTRA_KEY_DIRECTORY, directory);
+        close(RESULT_OK, i);
+    }
+
+    private void cancel() {
+        close(RESULT_CANCELED, null);
+    }
+
+    private void close(int resultCode, Intent i) {
+        setResult(resultCode, i);
+        finish();
+    }
+}
+
+/**
+ * vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
+ */
--- src/jp/gr/java_conf/neko_daisuki/simplemediascanner/MainActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/simplemediascanner/MainActivity.java
@@ -2,22 +2,184 @@ package jp.gr.java_conf.neko_daisuki.simplemediascanner;
 
 import java.io.File;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.media.MediaScannerConnection.MediaScannerConnectionClient;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private class DirectoryAdapter extends ArrayAdapter<Directory> {
+
+        private abstract class ListButtonOnClickListener implements View.OnClickListener {
+
+            protected int mPosition;
+
+            public ListButtonOnClickListener(int position) {
+                mPosition = position;
+            }
+
+            public abstract void onClick(View _);
+        }
+
+        private class DeleteButtonOnClickListener extends ListButtonOnClickListener {
+
+            public DeleteButtonOnClickListener(int position) {
+                super(position);
+            }
+
+            public void onClick(View _) {
+                //showConfirmDialogToDelete(mPosition);
+            }
+        }
+
+        private class RunButtonOnClickListener extends ListButtonOnClickListener {
+
+            public RunButtonOnClickListener(int position) {
+                super(position);
+            }
+
+            public void onClick(View _) {
+                Directory directory = mDirectories[mPosition];
+
+                // TODO
+                String fmt = "Started scanning for %s.";
+                Log.i(LOG_TAG, String.format(fmt, directory.path));
+                mConnection.connect();
+            }
+        }
+
+        private class EditButtonOnClickListener extends ListButtonOnClickListener {
+
+            public EditButtonOnClickListener(int position) {
+                super(position);
+            }
+
+            public void onClick(View _) {
+                Directory directory = mDirectories[mPosition];
+                startEditActivity(directory, RequestCode.EDIT);
+            }
+        }
+
+        private class Holder {
+
+            public TextView path;
+            public Button runButton;
+            public Button editButton;
+            public Button deleteButton;
+        }
+
+        private LayoutInflater mInflater;
+
+        public DirectoryAdapter(Context context, Directory[] objects) {
+            super(context, 0, objects);
+
+            String name = Context.LAYOUT_INFLATER_SERVICE;
+            mInflater = (LayoutInflater)context.getSystemService(name);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                return getView(position, makeView(parent), parent);
+            }
+            Holder holder = (Holder)convertView.getTag();
+            Directory directory = getItem(position);
+            holder.path.setText(directory.path);
+
+            holder.runButton.setOnClickListener(new RunButtonOnClickListener(position));
+            holder.editButton.setOnClickListener(new EditButtonOnClickListener(position));
+            holder.deleteButton.setOnClickListener(new DeleteButtonOnClickListener(position));
+
+            return convertView;
+        }
+
+        private TextView findTextView(View view, int id) {
+            return (TextView)view.findViewById(id);
+        }
+
+        private Button findButton(View view, int id) {
+            return (Button)view.findViewById(id);
+        }
+
+        private View makeView(ViewGroup parent) {
+            int layout = R.layout.list_row;
+            View convertView = mInflater.inflate(layout, parent, false);
+            Holder holder = new Holder();
+            holder.path = findTextView(convertView, R.id.directory_text);
+            holder.runButton = findButton(convertView, R.id.run_button);
+            holder.editButton = findButton(convertView, R.id.edit_button);
+            holder.deleteButton = findButton(convertView, R.id.delete_button);
+            convertView.setTag(holder);
+            return convertView;
+        }
+    }
+
+    private interface RequestCode {
+
+        public int ADD = 1;
+        public int EDIT = 2;
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        public interface Columns extends BaseColumns {
+
+            public static final String PATH = "path";
+        }
+
+        public static final String TABLE_NAME = "directories";
+
+        private static final String DATABASE_NAME = "directories.db";
+        private static final int DATABASE_VERSION = 1;
+
+        public DatabaseHelper(Context ctx) {
+            super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String fmt = "create table %s (%s integer primary key autoincrement"
+                + ", %s text not null);";
+            String sql = String.format(
+                    fmt,
+                    TABLE_NAME,
+                    Columns._ID, Columns.PATH);
+            db.execSQL(sql);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL(String.format("drop table if exists %s", TABLE_NAME));
+            onCreate(db);
+        }
+    }
+
     private class MediaScannerClient implements MediaScannerConnectionClient {
 
+        // TODO: A queue is better.
         private Deque<File> mFiles = new ArrayDeque<File>();
 
         public void onMediaScannerConnected() {
@@ -31,11 +193,11 @@ public class MainActivity extends Activity {
             scanNext();
         }
 
-        private void pushAll(String[] directories) {
+        private void pushAll(Directory[] directories) {
             int len = directories.length;
             File[] files = new File[len];
             for (int i = 0; i < len; i++) {
-                files[i] = new File(mDirectories[i]);
+                files[i] = new File(directories[i].path);
             }
             pushAll(files);
         }
@@ -68,30 +230,79 @@ public class MainActivity extends Activity {
         }
     }
 
+    private class AddButtonOnClickListener implements View.OnClickListener {
+
+        public void onClick(View _) {
+            startEditActivity(new Directory(), RequestCode.ADD);
+        }
+    }
+
     private class RunAllButtonOnClickListener implements View.OnClickListener {
 
         public void onClick(View _) {
-            startScanning();
+            Log.i(LOG_TAG, "Started scanning all directories.");
+            mConnection.connect();
+        }
+    }
+
+    private abstract class RequestProcedure {
+
+        public abstract void run(Directory directory);
+    }
+
+    private class EditRequestProcedure extends RequestProcedure {
+
+        public void run(Directory directory) {
+            SQLiteDatabase db = mDatabase.getWritableDatabase();
+            ContentValues values = makeContentValues(directory);
+            String where = String.format("%s=?", DatabaseHelper.Columns._ID);
+            String[] args = new String[] { Long.toString(directory.id) };
+            db.update(DatabaseHelper.TABLE_NAME, values, where, args);
+
+            updateDirectories();
+        }
+    }
+
+    private class AddRequestProcedure extends RequestProcedure {
+
+        public void run(Directory directory) {
+            SQLiteDatabase db = mDatabase.getWritableDatabase();
+            ContentValues values = makeContentValues(directory);
+            db.insertOrThrow(DatabaseHelper.TABLE_NAME, null, values);
+
+            updateDirectories();
         }
     }
 
     private static final String LOG_TAG = "simplemediascanner";
 
-    private String[] mDirectories = {
-        "/mnt/sdcard/radio",
-        "/mnt/sdcard/music" };
+    private Directory[] mDirectories;
+
+    private ListView mDirectoryList;
+
     private MediaScannerConnection mConnection;
+    private SQLiteOpenHelper mDatabase;
+    private SparseArray<RequestProcedure> mRequestProcedures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button runButton = (Button)findViewById(R.id.run_button);
-        runButton.setOnClickListener(new RunAllButtonOnClickListener());
+        mDirectoryList = (ListView)findViewById(R.id.directory_list);
+        Button runAllButton = (Button)findViewById(R.id.run_all_button);
+        runAllButton.setOnClickListener(new RunAllButtonOnClickListener());
+        Button addButton = (Button)findViewById(R.id.add_button);
+        addButton.setOnClickListener(new AddButtonOnClickListener());
 
         MediaScannerClient client = new MediaScannerClient();
         mConnection = new MediaScannerConnection(this, client);
+        mDatabase = new DatabaseHelper(this);
+        mRequestProcedures = new SparseArray<RequestProcedure>();
+        mRequestProcedures.put(RequestCode.ADD, new AddRequestProcedure());
+        mRequestProcedures.put(RequestCode.EDIT, new EditRequestProcedure());
+
+        updateDirectories();
     }
 
     @Override
@@ -101,10 +312,70 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    private void startScanning() {
-        Log.i(LOG_TAG, "Scanning started.");
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        String key = EditActivity.EXTRA_KEY_DIRECTORY;
+        Directory directory = (Directory)data.getSerializableExtra(key);
+        mRequestProcedures.get(requestCode).run(directory);
+    }
 
-        mConnection.connect();
+    private Directory[] queryDirectories() {
+        List<Directory> directories = new ArrayList<Directory>();
+
+        try {
+            SQLiteDatabase db = mDatabase.getReadableDatabase();
+            String[] columns = new String[] {
+                DatabaseHelper.Columns._ID,
+                DatabaseHelper.Columns.PATH };
+            Cursor cursor = db.query(
+                    DatabaseHelper.TABLE_NAME,
+                    columns,
+                    null,   // selection
+                    null,   // selection args
+                    null,   // group by
+                    null,   // having
+                    null);  // order by
+            try {
+                while (cursor.moveToNext()) {
+                    Directory directory = new Directory();
+                    directory.id = cursor.getLong(0);
+                    directory.path = cursor.getString(1);
+                    directories.add(directory);
+                }
+            }
+            finally {
+                cursor.close();
+            }
+        }
+        finally {
+            mDatabase.close();
+        }
+
+        return directories.toArray(new Directory[0]);
+    }
+
+    private void startEditActivity(Directory directory, int requestCode) {
+        Intent i = new Intent(this, EditActivity.class);
+        i.putExtra(EditActivity.EXTRA_KEY_DIRECTORY, directory);
+        startActivityForResult(i, requestCode);
+    }
+
+    private ContentValues makeContentValues(Directory directory) {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.Columns.PATH, directory.path);
+        return values;
+    }
+
+    private void updateList() {
+        mDirectoryList.setAdapter(new DirectoryAdapter(this, mDirectories));
+    }
+
+    private void updateDirectories() {
+        mDirectories = queryDirectories();
+        updateList();
     }
 }
 
