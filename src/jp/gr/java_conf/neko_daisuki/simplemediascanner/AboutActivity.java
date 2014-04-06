d56525b6daefd26c7ae75820066b9ad2e5a5549e
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-04-06 23:10:31 +0900

    Changed the AboutActivity to use an WebView.

--- src/jp/gr/java_conf/neko_daisuki/simplemediascanner/AboutActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/simplemediascanner/AboutActivity.java
@@ -1,23 +1,69 @@
 package jp.gr.java_conf.neko_daisuki.simplemediascanner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URLEncoder;
+
 import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.webkit.WebView;
 
 public class AboutActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_about);
-        this.showVersion();
+
+        String fmt = readTemplate();
+        String html = String.format(fmt, getVersion());
+        WebView view = new WebView(this);
+        view.loadData(URLEncoder.encode(html).replaceAll("\\+", "%20"), "text/html", "UTF-8");
+        setContentView(view);
     }
 
-    private void showVersion() {
-        PackageManager pm = this.getPackageManager();
-        String name = this.getPackageName();
+    private String readTemplate() {
+        StringBuilder buffer = new StringBuilder();
+        InputStream in;
+        try {
+            in = getAssets().open("license.html");
+            try {
+                Reader reader = new InputStreamReader(in);
+                try {
+                    BufferedReader br = new BufferedReader(reader);
+                    try {
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            buffer.append(line);
+                            buffer.append("\n");
+                        }
+                    }
+                    finally {
+                        br.close();
+                    }
+                }
+                finally {
+                    reader.close();
+                }
+            }
+            finally {
+                in.close();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        android.util.Log.d("x", buffer.toString());
+        return buffer.toString();
+    }
+
+    private String getVersion() {
+        PackageManager pm = getPackageManager();
+        String name = getPackageName();
         int flags = PackageManager.GET_INSTRUMENTATION;
 
         PackageInfo pi;
@@ -26,11 +72,10 @@ public class AboutActivity extends Activity {
         }
         catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
-            return;
+            return "???";
         }
 
-        TextView view = (TextView)this.findViewById(R.id.version);
-        view.setText(pi.versionName);
+        return pi.versionName;
     }
 }
 
